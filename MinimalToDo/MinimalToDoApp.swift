e6b5bd53c7cd71ac98347527d11b3c7f954a0849
gustavosouto02 <gugasouto2004@gmail.com>
2025-06-27 16:44:16 -0300

    Initial Commit

---
+++ MinimalToDo/MinimalToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalToDoApp.swift
+//  MinimalToDo
+//
+//  Created by Gustavo Souto Pereira on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MinimalToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
